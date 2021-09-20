0f8f24dd052d59ab6caa9a662e25f4207f7faf85
Sarankumar09 <90305889+Sarankumar09@users.noreply.github.com>
2021-09-21 00:49:02 +0530

    happy event source code
    Has the code with updated basic and guests tab forms

--- src/App.js
+++ src/App.js
@@ -1,124 +1,42 @@
-import { Component } from 'react';
-import {
-  Form,
-  FormFeedback,
-  FormGroup,
-  FormText,
-  Label,
-  Input,
-  Button,
-} from 'reactstrap';
+import logo from './logo.svg';
 import './App.css';
+import MenuListComposition from './Menulist';
+import MyEvent from './MyEvent';
+import MyProfile from './MyProfile';
+import ExploreEvent from './ExploreEvent';
+import EventHistory from './EventHistory';
+import AddButton from './PlusButton';
+import PrivateEvent from './PrivateEvent';
+import PublicEvent from './PublicEvent';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      validate: {
-        emailState: '',
-        passwordState: ''
-      },
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (event) => {
-    const { target } = event;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const { name } = target;
-
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  validateEmail(e) {
-    const emailRex =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    const { validate } = this.state;
-
-    if (emailRex.test(e.target.value)) {
-      validate.emailState = 'has-success';
-    } else {
-      validate.emailState = 'has-danger';
-    }
-
-    this.setState({ validate });
-  }
-
-  submitForm(e) {
-    e.preventDefault();
-    
-    console.log(`Email: ${this.state.email}`);
-  }
-
-  validatePassword(e){
-    
-    const { validate } = this.state;
-    console.log(validate);
-    if(e.target.value.length>=8) {
-      validate.passwordState = 'has-success';
-    } else {
-      validate.passwordState = 'has-danger';
-    }
-    console.log(validate);
-    this.setState({ validate });
-  }
-  
-
-  render() {
-    const { email, password } = this.state;
-    console.log(this.state);
-    return (
-      <div className="App">
-        <h2>Event Login</h2>
-        <Form className="form" onSubmit={(e) => this.submitForm(e)}>
-          <FormGroup>
-            <Label>Username</Label>
-            <Input
-              type="email"
-              name="email"
-              id="exampleEmail"
-              placeholder="example@example.com"
-              valid={this.state.validate.emailState === "has-success"}
-              invalid={this.state.validate.emailState === "has-danger"}
-              value={email}
-              onChange={(e) => {
-                this.validateEmail(e);
-                this.handleChange(e);
-              }}
-            />
-            <FormFeedback>
-              Uh oh! Looks like there is an issue with your email. Please input
-              a correct email.
-            </FormFeedback>
-            <FormFeedback valid>
-              That's a tasty looking email you've got there.
-            </FormFeedback>
-            <FormText>Your username is most likely your email.</FormText>
-          </FormGroup>
-          <FormGroup>
-            <Label for="examplePassword">Password</Label>
-            <Input
-              type="password"
-              name="password"
-              id="examplePassword"
-              placeholder="password"
-              valid={this.state.validate.passwordState === "has-success"}
-              invalid={this.state.validate.passwordState === "has-danger"}
-              
-              value={password}
-              onChange={(e) => {
-                this.validatePassword(e);
-                this.handleChange(e)}}
-            />
-          </FormGroup>
-          <Button disabled={!this.state.email||!this.state.password}>Submit</Button>
-        </Form>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <h1>Happy Go Lucky</h1>
+       
+      <MenuListComposition/>
+      <AddButton/>
+      <Switch> 
+        <Route path="/MyProfile" component={MyProfile} />
+        <Route path="/MyEvent" component={MyEvent} />
+        <Route path="/ExploreEvent" component={ExploreEvent} />
+        <Route path="/EventHistory" component={EventHistory} />
+        <Route path="/PrivateEvent" component={PrivateEvent} />
+        <Route path="/PublicEvent" component={PublicEvent} />
+      </Switch>
+    </div>
+    </Router>
+  );
 }
+
+export default App;
